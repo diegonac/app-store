fcfebf4902bf3aabf9eed2a1c44c6e955a9fd0a5
Diego Nacimiento <diegonacimiento18@gmail.com>
2022-08-20 20:12:54 -0300

    Servicio terminado

--- routes/productos.router.js
+++ routes/productos.router.js
@@ -1,90 +1,84 @@
 import express from "express";
+import productosService from "../services/productos.services.js";
 const router = express.Router();
+const service = new productosService();
 
-const productos = {
-	100: {
-				Nombre: "iPhone 13",
-				Precio: "$300.000",
-				},
-	101: {
-				Nombre: "Samsung Galaxy S22",
-				Precio: "$270.000",
-				},
-};
-
-// Creamos un endpoint con el método get para que nos muestre los productos:
 router.get("/", (req, res) => {
-  res.json(productos);
+  res.json(service.buscar());
 });
-// Creamos otro endpoint con el método get para que nos muestre un producto en específico:
 router.get("/:id", (req, res) => {
-  const { id } = req.params;
-	if (productos[`${id}`]) {
-			res.json(productos[`${id}`]);
-	} else {
-			res.status(404).send("Lo siento el producto no existe");
-	};
+  try {
+    const { id } = req.params;
+	  const producto = service.buscarId(id);
+    res.json(producto);
+  } catch (error) {
+    res.status(404).json({
+      message: error
+    });
+  };
 });
 
-// Creamos otro endpoint con el método post para crear productos:
-router.post("/:id", (req, res) => {
-	const { id } = req.params;
-	const body = req.body;
-	productos[`${id}`] = body;
-  res.status(201).json({
-    message: "Creado",
-    body,
-    id,
-  });
+router.post("/crear", (req, res) => {
+  try {
+    const body = req.body;
+    const producto = service.crear(body);
+    res.status(201).json({
+      message: "Creado",
+      producto,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error,
+    });
+  };
+
 });
 
-// Creamos un endpoint con el método put para modificar los productos:
 router.put("/:id", (req, res) => {
-	const { id } = req.params;
-	const body = req.body;
-	if (productos[`${id}`]) {
-		productos[`${id}`] = body;
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const producto = service.modificar(id, body);
 		res.json({
 		message: "Modificado",
-		body,
-		id,
+		producto,
 		});
-	} else {
-			res.status(404).send(`Lo siento, el producto que usted quiere modificar no
-			existe`);
-	};
+  } catch (error) {
+    res.status(404).json({
+      message: error,
+    });
+  }
 });
 
-// Creamos un endpoint con el método patch para modificar parcialmente un
-// producto:
 router.patch("/:id/:propiedad", (req, res) => {
-	const { id, propiedad } = req.params;
-	const body = req.body;
-	if (productos[`${id}`][`${propiedad}`]) {
-		productos[`${id}`][`${propiedad}`] = body;
-		res.json({
-		message: "Modificado",
-		body,
-		id,
-		});
-	} else {
-			res.status(404).send(`Lo siento, el producto o el atributo que usted quiere
-			modificar no existe`);
-	};
+  try {
+    const { id, propiedad } = req.params;
+    const body = req.body;
+    const producto = service.modificarAtributo(id, propiedad, body);
+    res.json({
+      message: "Modificado",
+      producto,
+      });
+  } catch (error) {
+    res.status(404).json({
+      message: error,
+    });
+  };
 });
 
-// Creamos un endpoint con el método delete para eliminar productos:
 router.delete("/:id", (req, res) => {
-	const { id } = req.params;
-	if (productos[`${id}`]) {
-		delete productos[`${id}`];
+  try {
+    const { id } = req.params;
+		const producto = service.eliminar(id);
 		res.json({
 		message: "Eliminado",
-		id,
+		producto,
 	});
-	} else {
-		res.status(404).send("Lo siento, el producto que usted busca no existe");
-	};
+  } catch (error) {
+		res.status(404).json({
+      message: error,
+    });
+  };
 });
 
 export default router;
--- services/productos.services.js
+++ services/productos.services.js
@@ -1,40 +1,70 @@
 class productosService {
   constructor() {
-    this.productos = {
-      100: {
-        Nombre: "iPhone 13",
-        Precio: "$300.000",
-      },
-      101: {
-        Nombre: "Samsung Galaxy S22",
-        Precio: "$270.000",
-      },
-    };
+    this.productos = [{
+      Nombre: "iPhone 13",
+      Precio: "$300.000",
+      id: 100,
+    },{
+      Nombre: "Samsung Galaxy S22",
+      Precio: "$270.000",
+      id: 101,
+    }];
   };
 
   buscar() {
     return this.productos;
   };
 
-  buscarId() {
-
+  buscarId(id) {
+    const index = this.productos.findIndex(index => index.id == id);
+    if (index == -1) {
+      throw "El producto no se ha encontrado";
+    };
+    return this.productos[index];
   };
 
-  crear() {
-
+  crear(body) {
+    const id = body["id"];
+    const index = this.productos.findIndex(index => index.id == id);
+    if (index != -1) {
+      throw "El producto ya existe, coloque otro id para crear un nuevo producto";
+    };
+    this.productos.push(body);
+    return body;
   };
 
-  modificar() {
-
+  modificar(id, body) {
+    const index = this.productos.findIndex(index => index.id == id);
+    if (index == -1) {
+      throw "El producto no existe";
+    };
+    const producto = this.productos[index];
+    this.productos[index] = {
+      ...producto,
+      ...body,
+    };
+    return this.productos[index];
   };
 
-  modificarAtributo() {
-
+  modificarAtributo(id, propiedad, body) {
+    const index = this.productos.findIndex(index => index.id == id);
+    if (index == -1 || !this.productos[index][`${propiedad}`]) {
+      throw "El producto o el atributo seleccionado no existe"
+    };
+    this.productos[index][`${propiedad}`] = body;
+    return this.productos[index];
   };
 
-  eliminar() {
-
+  eliminar(id) {
+    const index = this.productos.findIndex(index => index.id == id);
+    if (index == -1) {
+      throw "El producto no existe";
+    };
+    const producto = this.productos[index];
+    this.productos.splice(index, 1);
+    return producto;
   };
 };
 
 export default productosService;
+
