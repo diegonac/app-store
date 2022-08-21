96b08d6535e88442222da2525800c099548f866d
Diego Nacimiento <diegonacimiento18@gmail.com>
2022-08-20 21:00:21 -0300

    Servicios hechos

--- routes/productos.router.js
+++ routes/productos.router.js
@@ -3,82 +3,82 @@ import productosService from "../services/productos.services.js";
 const router = express.Router();
 const service = new productosService();
 
-router.get("/", (req, res) => {
-  res.json(service.buscar());
-});
-router.get("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-	  const producto = service.buscarId(id);
-    res.json(producto);
-  } catch (error) {
-    res.status(404).json({
-      message: error
-    });
-  };
+router.get("/", async (req, res) => {
+	res.json(await service.buscar());
 });
 
-router.post("/crear", (req, res) => {
-  try {
-    const body = req.body;
-    const producto = service.crear(body);
-    res.status(201).json({
-      message: "Creado",
-      producto,
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: error,
-    });
-  };
-
-});
-
-router.put("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const producto = service.modificar(id, body);
-		res.json({
-		message: "Modificado",
-		producto,
-		});
-  } catch (error) {
-    res.status(404).json({
-      message: error,
-    });
-  }
-});
-
-router.patch("/:id/:propiedad", (req, res) => {
-  try {
-    const { id, propiedad } = req.params;
-    const body = req.body;
-    const producto = service.modificarAtributo(id, propiedad, body);
-    res.json({
-      message: "Modificado",
-      producto,
-      });
-  } catch (error) {
-    res.status(404).json({
-      message: error,
-    });
-  };
-});
-
-router.delete("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-		const producto = service.eliminar(id);
-		res.json({
-		message: "Eliminado",
-		producto,
-	});
-  } catch (error) {
+router.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const producto = await service.buscarId(id);
+		res.json(producto);
+	} catch (error) {
 		res.status(404).json({
-      message: error,
-    });
-  };
+			message: error,
+		});
+	};
 });
+
+	router.post("/crear", async (req, res) => {
+		try {
+			const body = req.body;
+			const producto = await service.crear(body);
+			res.status(201).json({
+				message: "Creado",
+				producto,
+			});
+		} catch (error) {
+				res.status(404).json({
+					message: error,
+				});
+		};
+	});
+
+	router.put("/:id", async (req, res) => {
+		try {
+			const { id } = req.params;
+			const body = req.body;
+			const producto = await service.modificar(id, body);
+			res.json({
+				message: "Modificado",
+				producto,
+			});
+		} catch (error) {
+			res.status(404).json({
+				message: error,
+			});
+		};
+	});
+
+	router.patch("/:id/:propiedad", async (req, res) => {
+		try {
+			const { id, propiedad } = req.params;
+			const body =  req.body;
+			const producto = await service.modificarAtributo(id, propiedad, body);
+			res.json({
+				message: "Modificado",
+				producto,
+			});
+		} catch (error) {
+			res.status(404).json({
+				meesage: error,
+			});
+		};
+	});
+
+	router.delete("/:id", async (req, res) => {
+		try {
+			const { id } = req.params;
+			const producto = await service.eliminar(id);
+			res.json({
+				message: "Eliminado",
+				producto,
+			});
+		} catch (error) {
+				res.status(404).json({
+					meesage: error,
+				});
+		};
+	});
 
 export default router;
--- services/productos.services.js
+++ services/productos.services.js
@@ -1,70 +1,92 @@
+// Creamos una clase con todos los métodos que vamos a utilizar:
 class productosService {
   constructor() {
-    this.productos = [{
-      Nombre: "iPhone 13",
-      Precio: "$300.000",
-      id: 100,
-    },{
-      Nombre: "Samsung Galaxy S22",
-      Precio: "$270.000",
-      id: 101,
-    }];
+		// El objeto de productos lo creamos aquí:
+		// En esta ocación utilizaremos un array:
+		this.productos = [
+			{Nombre: "iPhone 13", Precio: "$270.000", id: "100"},
+			{Nombre: "Samsung Galaxy S22", Precio: "$240.000", id:"101"},
+		];
   };
 
-  buscar() {
-    return this.productos;
+  async buscar() {
+		// Solamente se returna el array de productos:
+		return this.productos;
   };
 
-  buscarId(id) {
-    const index = this.productos.findIndex(index => index.id == id);
-    if (index == -1) {
-      throw "El producto no se ha encontrado";
-    };
-    return this.productos[index];
+  async buscarId(id) {
+		// Recibe como parámetro el id y luego:
+		// (1)
+		const index = this.productos.findIndex(index => index.id == id);
+
+		// Verificamos si existe el producto
+		if (index == -1) {
+			throw "El producto no existe";
+		};
+
+		// Si existe no entrará en el if por lo tanto retornamos el
+		// elemento:
+		return this.productos[index];
   };
 
-  crear(body) {
-    const id = body["id"];
-    const index = this.productos.findIndex(index => index.id == id);
-    if (index != -1) {
-      throw "El producto ya existe, coloque otro id para crear un nuevo producto";
-    };
-    this.productos.push(body);
-    return body;
+  async crear(body) {
+		// Recibe el body y luego:
+		// Guardamos el id del body:
+		const id = body["id"];
+		const index = this.productos.findIndex(index => index.id == id);
+
+		// Comprobamos que no exista un producto con dicha id:
+		// (2)
+		if (index != -1) {
+			throw "El producto ya existe, utilice otro id";
+		};
+
+		// Si el producto no existe en el array, lo creamos:
+		this.productos.push(body);
+		return body;
   };
 
-  modificar(id, body) {
-    const index = this.productos.findIndex(index => index.id == id);
-    if (index == -1) {
-      throw "El producto no existe";
-    };
-    const producto = this.productos[index];
-    this.productos[index] = {
-      ...producto,
-      ...body,
-    };
-    return this.productos[index];
+  async modificar(id, body) {
+		// Recibe como parámetro un id y un body, luego:
+		const index = this.productos.findIndex(index => index.id == id);
+
+		if (index == -1) {
+			throw "El producto no existe";
+		};
+
+		// (3)
+		const producto = this.productos[index];
+		this.productos[index] = {
+			...producto,
+			...body,
+		};
+
+		return this.productos[index];
   };
 
-  modificarAtributo(id, propiedad, body) {
-    const index = this.productos.findIndex(index => index.id == id);
-    if (index == -1 || !this.productos[index][`${propiedad}`]) {
-      throw "El producto o el atributo seleccionado no existe"
-    };
-    this.productos[index][`${propiedad}`] = body;
-    return this.productos[index];
-  };
+  async modificarAtributo(id, propiedad, body) {
+		// Recibe los parámetros id, propiedad y body, luego:
+		const index = this.productos.findIndex(index => index.id == id);
 
-  eliminar(id) {
-    const index = this.productos.findIndex(index => index.id == id);
-    if (index == -1) {
-      throw "El producto no existe";
-    };
-    const producto = this.productos[index];
-    this.productos.splice(index, 1);
-    return producto;
+		// Verifico que el producto y la propidad a modificar existan:
+		if (index == -1 || !this.productos[index][`${propiedad}`]) {
+			throw "El producto o el atributo seleccionado no existe";
+		};
+
+		this.productos[index][`${propiedad}`] = body;
+		return this.productos[index];
+	};
+
+  async eliminar(id) {
+		const index = this.productos.findIndex(index => index.id == id);
+		if (index == -1) {
+			throw "El producto no existe";
+		};
+		const producto = this.productos[index];
+		this.productos.splice(index, 1);
+		return producto;
   };
 };
 
+// Exportamos la clase:
 export default productosService;
-
