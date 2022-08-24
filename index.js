1ed747be97a96e783ab49bf0bc6d22f889c55445
Diego Nacimiento <diegonacimiento18@gmail.com>
2022-08-24 12:04:49 -0300

    app store terminada

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import routerApi from "./routes/index.js";
-
-// Importamos la función del boom:
 import { logErr, errorHandler, boomErrorHandler } from "./middlewares/error.handler.js";
+
+// Importamos cors:
+import cors from "cors";
+
 const expressApp = express();
 const PORT = 3000;
 expressApp.listen(PORT, () => {
@@ -12,10 +14,23 @@ expressApp.listen(PORT, () => {
 expressApp.use(express.json());
 expressApp.use(express.text());
 
+// Creamos un array con los dominios que le daremos permisos:
+const whitelist = ["http://localhost:8000", "https://unejemplocualquiera.com"];
+// Creamos un objeto que verifique los permisos:
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido"));
+    }
+  }
+};
+// Le damos un use(options) a cors:
+expressApp.use(cors(options));
 
 routerApi(expressApp);
 
-// Aplicamos el método use() al boom en segundo lugar:
 expressApp.use(logErr);
 expressApp.use(boomErrorHandler);
 expressApp.use(errorHandler);
